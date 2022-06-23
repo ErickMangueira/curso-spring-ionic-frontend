aa68bcd4d4e1b852dcc27a6c9008f4f802be4ba4
Erick Mangueira <erickmangueira@gmail.com>
2022-06-23 19:07:57 -0300

    Acesso ao carrinho: botao flutuante e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AuthService } from '../services/Auth.service';
-
-
-
+import { AuthService } from '../services/auth.service';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,22 +10,24 @@ import { AuthService } from '../services/Auth.service';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: string = "HomePage";
+  rootPage: string = 'HomePage';
 
   pages: Array<{title: string, component: string}>;
 
-  constructor(public platform: Platform, 
-    public statusBar: StatusBar,
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
     public splashScreen: SplashScreen,
-    public auth: AuthService) {
+    public auth: AuthService
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Profile', component: "ProfilePage" },
-      { title: 'Categorias', component: "CategoriasPage" },
-      {title: 'Logout', component: ''}
-     
+      { title: 'Profile', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Carrinho', component: 'CartPage'},
+      { title: 'Logout', component: ''}
     ];
 
   }
@@ -42,16 +41,16 @@ export class MyApp {
     });
   }
 
-  openPage(page: {title:string, component:string}) {
+  openPage(page : {title:string, component:string}) {
 
-    switch (page.title){
-      case 'Logout' :
-        this.auth.logout();
-        this.nav.setRoot('HomePage');
-        break;
+    switch (page.title) {
+      case 'Logout':
+      this.auth.logout();
+      this.nav.setRoot('HomePage');
+      break;
 
+      default:
+      this.nav.setRoot(page.component);
     }
-   
-    this.nav.setRoot(page.component);
   }
 }
