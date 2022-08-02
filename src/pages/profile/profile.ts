b534ca1f674f9675f27778ea949a942486121ef5
Erick Mangueira <erickmangueira@gmail.com>
2022-08-02 17:28:43 -0300

    Buscando endereços do banco de dados

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { API_CONFIG } from '../../config/api.config';
+import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
-import { StorageService } from '../../services/storage.service';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -19,28 +12,28 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
- cliente : ClienteDTO;
+  cliente: ClienteDTO;
 
-  constructor(public navCtrl: NavController, 
+  constructor(
+    public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService : ClienteService) {
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email){
+    if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.cliente = response;
-        this.getImageIfExists();
-
-      },
-       error => {
-         if (error.status == 403){
-           this.navCtrl.setRoot('HomePage');
-         }
-       });
+        .subscribe(response => {
+          this.cliente = response as ClienteDTO;
+          this.getImageIfExists();
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
     }
     else {
       this.navCtrl.setRoot('HomePage');
@@ -54,5 +47,4 @@ export class ProfilePage {
     },
     error => {});
   }
-
 }
